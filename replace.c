5a50e1e01c0254d031ca7af87edd9504e4d8d77f
mcclusky <samantharobertz@outlook.com>
2026-04-09 20:54:26 +0700

    Add files via upload

---
+++ replace.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> 
+#define SIZE 100
+
+int main() {
+    char str[SIZE];
+    printf("Enter your word here: ");
+    scanf("%[^\n]", str);
+
+
+     char replaceword[SIZE];
+    printf("What is the word you want to be replaced? \n");
+    scanf("%s", replaceword);
+
+    
+    char replaced[SIZE];
+    printf("The words you want to replace: \n");
+    scanf("%s", replaced);
+
+    while (1) {
+        char *pos = strstr(str, replaceword);
+
+        if (pos != NULL) {
+            strncpy(pos, replaced, strlen(replaced));
+        } else {
+            break;
+        }
+
+    }
+
+    printf("%s\n", str);
+
+    char *p= str;
+    for (int i=0; p[i] !='\0'; i++) {
+        if (islower(p[i])) {
+            p[i]=toupper(p[i]);
+        }
+    }
+    printf("Converted string is: %s\n", str);
+
+    return 0;
+
+}
